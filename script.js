cb4a6401daa7d46b2b42a994f17ec9e49c262262
Nonso-charles <163753641+Nonso-charles@users.noreply.github.com>
2024-12-09 12:52:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Create a showSideBar function
+function showSideBar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSideBar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
